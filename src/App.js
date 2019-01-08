ea99d7bb93b79ae59958e895ccd64f6ce4e3cb4c
Richard Nabaasa <nabaasarichard@gmail.com>
2019-01-08 10:43:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eos ut
+          quia obcaecati quis sed, fuga sapiente labore, tempora corporis facere
+          sit praesentium voluptas. Nesciunt laudantium laborum, sapiente quod
+          possimus!
+        </p>
+        <br />
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
